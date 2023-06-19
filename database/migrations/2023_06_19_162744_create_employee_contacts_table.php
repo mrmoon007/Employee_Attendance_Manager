49f446f974a63c8d2fa13d69e7c587ccb09e98e6
mostafijur007 <mostafijur.techvill@gmail.com>
2023-06-19 22:59:13 +0600

    added employee contact migration

--- database/migrations/2023_06_19_162744_create_employee_contacts_table.php
+++ database/migrations/2023_06_19_162744_create_employee_contacts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('employee_contacts', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('employee_id')->index();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
