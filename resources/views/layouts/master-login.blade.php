fc0c7e65a1160b9fb0dd349fa251fa2ca9ebe3d8
mostafijur007 <mostafijur.techvill@gmail.com>
2023-06-21 22:23:06 +0600

    template martering

---
+++ resources/views/layouts/master-login.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="admin, dashboard">
+	<meta name="author" content="DexignZone">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Dompet : Payment Admin Template">
+	<meta property="og:title" content="Dompet : Payment Admin Template">
+	<meta property="og:description" content="Dompet : Payment Admin Template">
+	<meta property="og:image" content="https://dompet.dexignlab.com/xhtml/social-image.png">
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Dompet : Payment Admin Template</title>
+	
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+    <link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('admin/vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('admin/js/custom.min.js') }}"></script>
+    <script src="{{ asset('admin/js/dlabnav-init.js') }}"></script>
+	<script src="{{ asset('admin/js/styleSwitcher.js') }}"></script>
+</body>
+</html>
